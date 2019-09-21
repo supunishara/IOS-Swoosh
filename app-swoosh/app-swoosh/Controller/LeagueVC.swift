921a787052590175b53c239e8c71f23e2a87069b
Supun Ishara <supun.ishara@i4tradies.com>
2019-09-21 21:02:14 +0530

    created Player Data Model

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
     
+    @IBOutlet weak var nextBtn: BorderButton!
+     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        player = Player();
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens");
+        
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens");
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed");
+    }
+    
+    func selectLeague(leagueType:String){
+        player.desiredLeague = leagueType;
+        nextBtn.isEnabled = true;
     }
     
     //037 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Supun Ishara  on 9/21/19.
+//  Copyright © 2019 vimukthi visvanath. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
